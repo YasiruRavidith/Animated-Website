d893a3ed765b6366840c9cd71088694acf1ca846
YasiruRavidith <yasiruravidith123@gmail.com>
2025-03-04 22:48:56 +0530

    About

---
+++ src/components/AnimatedTitle.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AnimatedTitle = () => {
+  return (
+    <div>
+        <div className='mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]'>
+                Dive str<b>a</b>ight int<b>o</b> the <br/>fr<b>a</b>y
+
+            </div>
+    </div>
+  )
+}
+
+export default AnimatedTitle
